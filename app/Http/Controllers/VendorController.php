58f7435b57e359bae3718694f751d223abb4b545
Maurice Mwanza <mauricedevs@gmail.com>
2024-01-16 08:03:10 +0300

    vendor dashboard

--- app/Http/Controllers/VendorController.php
+++ app/Http/Controllers/VendorController.php
@@ -10,4 +10,10 @@ class VendorController extends Controller
     public function VendorDashboard(){
         return view('vendor.index');
     }// End Vendor Dashboard
+
+
+    public function VendorLogin(){
+        return view('vendor.vendor_login');
+    }//End Admin Login
+
 }
--- routes/web.php
+++ routes/web.php
@@ -46,4 +46,5 @@ Route::middleware(['auth', 'role:vendor'])->group(function(){
 
 
 Route::get('/admin/login', [AdminController::class, 'AdminLogin']);
+Route::get('/vendor/login', [VendorController::class, 'VendorLogin']);
 
